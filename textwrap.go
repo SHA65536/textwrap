7647d65092c5d3b66db1d8dc1391e7f1772dee13
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-08-26 22:25:53 +0300

    first functions and tests

--- textwrap.go
+++ textwrap.go
@@ -1,1 +1,49 @@
 package textwrap
+
+import "strings"
+
+const defaultDelim string = " "
+const defaultEnd string = "\n"
+
+func WrapCustom(input string, size int, delim, end string) ([]string, error) {
+	var results = []string{}
+	var strs = strings.Split(input, end)
+	for _, str := range strs {
+		if len(str) <= size {
+			results = append(results, str)
+			continue
+		}
+		var curStr string
+		words := strings.Split(str, delim)
+		for _, word := range words {
+			if len(curStr)+len(delim)+len(word) > size {
+				if curStr != "" {
+					results = append(results, curStr)
+				}
+				for len(word) > size {
+					results = append(results, word[:size])
+					word = word[size:]
+				}
+				curStr = word
+			} else {
+				if curStr == "" {
+					curStr = word
+				} else {
+					curStr += delim + word
+				}
+			}
+		}
+		if curStr != "" {
+			results = append(results, curStr)
+		}
+	}
+	return results, nil
+}
+
+func Wrap(input string, size int) (string, error) {
+	res, err := WrapCustom(input, size, defaultDelim, defaultEnd)
+	if err != nil {
+		return "", err
+	}
+	return strings.Join(res, "\n"), nil
+}
--- textwrap_test.go
+++ textwrap_test.go
@@ -1,1 +1,27 @@
 package textwrap
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed testdata/bee_raw.txt
+var beeRaw string
+
+//go:embed testdata/bee_16.txt
+var bee16 string
+
+//go:embed testdata/bee_8.txt
+var bee8 string
+
+func TestRegularWrap(t *testing.T) {
+	assert := assert.New(t)
+	result, _ := Wrap(beeRaw, 16)
+	assert.Equal(bee16, result, fmt.Sprintf("%s\n\n===should equal===\n\n%s", result, bee16))
+
+	result, _ = Wrap(beeRaw, 8)
+	assert.Equal(bee8, result, fmt.Sprintf("%s\n\n===should equal===\n\n%s", result, bee8))
+}
